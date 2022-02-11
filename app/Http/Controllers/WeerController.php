465b3cb7f0de689494a4333853ba5a335189f16e
Kimberly Hogeveen <2103405@talnet.nl>
2022-02-11 15:29:36 +0100

    request added

--- app/Http/Controllers/WeerController.php
+++ app/Http/Controllers/WeerController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class WeerController extends Controller
 {
     public function index()
     {
-        echo 'hello';
+        $response = Http::get('https://api.tomorrow.io/v4/timelines?location=-73.98529171943665,40.75872069597532&f[…]steps=1h&units=metric&apikey=65W7qEjsqGm69G2o97zz1ZkU2MlQb12u
+');
+        dd($response);
     }
 }
